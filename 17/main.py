4ea66f992250fa7eddb862a0af6a53861b60f342
MatiasStorm <mat-435@hotmail.com>
2020-12-18 09:38:06 +0100

    Update day 17

--- 17/main.py
+++ 17/main.py
@@ -1,81 +1,49 @@
+import time
+
 def get_active(start):
-    return [(0,y,x) for y in range(len(start)) for x in range(len(start[0])) if start[y][x] == "#"]
+    return [(0, y, x, 0) for y in range(len(start)) for x in range(len(start[0])) if start[y][x] == "#"]
 
-def get_active4d(start):
-    return [(0,y,x,0) for y in range(len(start)) for x in range(len(start[0])) if start[y][x] == "#"]
+def get_inactive(active, part2=False):
+    inactive = []
+    for cube in active:
+        inactive += get_neighbours(cube, part2)
+    return set(inactive)
 
-def get_neighbours(z, y, x):
+def get_neighbours(cube, part2):
     neighbours  = []
+    z, y, x, w = cube
     for zs in range(z - 1, z + 2):
         for ys in range(y - 1, y + 2):
             for xs in range(x - 1, x + 2):
-                if not ( xs == x and ys == y and zs == z ):
-                    neighbours.append(( zs, ys, xs ))
+                if part2:
+                    for ws in range(w-1, w+2):
+                        if not ( xs == x and ys == y and zs == z and ws == w ):
+                            neighbours.append(( zs, ys, xs, ws ))
+                elif not ( xs == x and ys == y and zs == z ):
+                    neighbours.append(( zs, ys, xs, 0 ))
     return neighbours
 
-def get_neighbours4d(z, y, x, w):
-    neighbours  = []
-    for zs in range(z - 1, z + 2):
-        for ys in range(y - 1, y + 2):
-            for xs in range(x - 1, x + 2):
-                for ws in range(w-1, w+2):
-                    if not ( xs == x and ys == y and zs == z and ws == w ):
-                        neighbours.append(( zs, ys, xs, ws ))
-    return neighbours
-
-def get_ranges(active):
-    zs = [a[0] for a in active]
-    ys = [a[1] for a in active]
-    xs = [a[2] for a in active]
-    return (range(min(zs) - 1, max(zs) + 2), range(min(ys) - 1, max(ys) + 2), range(min(xs) - 1, max(xs) + 2))
-
-def get_ranges4d(active):
-    zs = [a[0] for a in active]
-    ys = [a[1] for a in active]
-    xs = [a[2] for a in active]
-    ws = [a[3] for a in active]
-    return (range(min(zs) - 1, max(zs) + 2)
-            , range(min(ys) - 1, max(ys) + 2)
-            , range(min(xs) - 1, max(xs) + 2)
-            , range(min(ws) - 1, max(ws) + 2)
-            )
+def get_number_of_active(start, part2=False):
+    active = get_active(start)
+    for _ in range(6):
+        new_active = active.copy()
+        inactive = get_inactive(active, part2)
+        for cube in active:
+            count = len([i for i in get_neighbours(cube, part2) if i in active])
+            if count not in (2,3) and cube in active:
+                new_active.remove(cube)
+        for cube in inactive:
+            count = len([i for i in get_neighbours(cube, part2) if i in active])
+            if count == 3 and cube not in active:
+                new_active.append(cube)
+        active = new_active
+    return len(active)
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         start = [l.strip() for l in f.readlines()]
-    active = get_active(start)
 
-    for _ in range(6):
-        ranges = get_ranges(active)
-        new_active = active.copy()
-        for z in ranges[0]:
-            for y in ranges[1]:
-                for x in ranges[2]:
-                    cube = (z,y,x)
-                    count = len([i for i in get_neighbours(z, y, x) if i in active])
-                    if count not in (2,3) and cube in active:
-                        new_active.remove(cube)
-                    elif count == 3 and cube not in active:
-                        new_active.append(cube)
-        active = new_active
-    print(len(active))
-                         
-    active = get_active4d(start)
-    for _ in range(6):
-        print(len(active))
-        ranges = get_ranges4d(active)
-        new_active = active.copy()
-        for z in ranges[0]:
-            for y in ranges[1]:
-                for x in ranges[2]:
-                    for w in ranges[3]:
-                        cube = (z,y,x, w)
-                        count = len([i for i in get_neighbours4d(z, y, x, w) if i in active])
-                        if count not in (2,3) and cube in active:
-                            new_active.remove(cube)
-                        elif count == 3 and cube not in active:
-                            new_active.append(cube)
-        active = new_active
-    print(len(active))
+    print("Part 1", get_number_of_active(start))
+    print("Part 2", get_number_of_active(start, True))
 
     
