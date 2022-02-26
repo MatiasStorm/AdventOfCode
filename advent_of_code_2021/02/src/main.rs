ce544969876db38adb2f4f26575aa5d2249d2ad3
MatiasStorm <mat-435@hotmail.com>
2022-02-26 11:40:33 +0100

    Finish 2021 02

--- advent_of_code_2021/02/src/main.rs
+++ advent_of_code_2021/02/src/main.rs
@@ -6,24 +6,60 @@ fn get_input() -> Vec<Command>{
     let mut content = fs::read_to_string("input.txt").expect("Could not read file");
     content.pop(); // remove trailing '\n'
     return content.split("\n").map(|row| { 
-        let mut data = row.split("\n");
-        let cmd = data.nth(0).unwrap().to_string();
-        let dist = data.nth(1).unwrap().parse::<i32>().unwrap();
+        let mut data = row.split(" ");
+        let cmd;
+        match data.nth(0) {
+            Some(command) => cmd = command.to_string(),
+            None => panic!("No command..."),
+        }
+        let dist;
+        match data.nth(0) {
+            Some(string) => {
+                match string.parse::<i32>() {
+                    Ok(value) => dist = value,
+                    Err(e) => panic!("Error parsing int: {:?}", e),
+                }
+            },
+            None => panic!("No command value...")
+        }
         return (cmd, dist) 
     }).collect();
 }
 
-fn first(commands:Vec<Command>) -> i32 {
+fn first(commands:&Vec<Command>) -> i32 {
     let mut d = 0;
     let mut h = 0;
-    for (cmd,n) in commands.iter() {
-        match *cmd{
-            "forward": h += cmd.1,
+    for cmd in commands {
+        match cmd.0.as_str(){
+            "forward" => h += cmd.1,
+            "up" => d -= cmd.1,
+            "down" => d += cmd.1,
+            _ => println!("Unknown command {}", cmd.0)
         }
     }
+    return d * h;
+}
+
+fn second(commands:&Vec<Command>) -> i32 {
+    let mut d = 0;
+    let mut h = 0;
+    let mut a = 0;
+    for cmd in commands {
+        match cmd.0.as_str(){
+            "forward" => { 
+                h += cmd.1;
+                d += cmd.1 * a;
+            },
+            "up" => a -= cmd.1,
+            "down" => a += cmd.1,
+            _ => println!("Unknown command {}", cmd.0)
+        }
+    }
+    return d * h;
 }
 
 fn main() {
     let commands = get_input();
-    println!("1: {}", first(commands));
+    println!("1: {}", first(&commands));
+    println!("2: {}", second(&commands));
 }
