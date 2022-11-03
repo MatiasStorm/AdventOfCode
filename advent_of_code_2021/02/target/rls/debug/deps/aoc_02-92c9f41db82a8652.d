e5f6e43c3c0d3f82844e2298d49b4a8edbb9bef2
Matias Storm <matias.storm42@gmail.com>
2022-11-03 07:21:06 +0100

    Rename year folders

--- advent_of_code_2021/02/target/rls/debug/deps/aoc_02-92c9f41db82a8652.d
+++
@@ -1,5 +0,0 @@
-/home/matias/j/git/advent_of_code/advent_of_code_2021/02/target/rls/debug/deps/aoc_02-92c9f41db82a8652.rmeta: src/main.rs
-
-/home/matias/j/git/advent_of_code/advent_of_code_2021/02/target/rls/debug/deps/aoc_02-92c9f41db82a8652.d: src/main.rs
-
-src/main.rs:
