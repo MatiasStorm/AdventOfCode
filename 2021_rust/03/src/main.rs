68cbe382f2ef4dc984fec83c199ce52daca789fc
Matias Storm <matias.storm42@gmail.com>
2022-12-27 15:38:42 +0100

    Add readme to 2021

--- 2021_rust/03/src/main.rs
+++ 2021_rust/03/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+
+
+
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn get_input() -> Vec<Command>{
+    let mut content = fs::read_to_string("input.txt").expect("Could not read file");
+    content.pop(); // remove trailing '\n'
+    return content.split("\n").map(|row| { 
+        let mut data = row.split(" ");
+        let cmd;
+        match data.nth(0) {
+            Some(command) => cmd = command.to_string(),
+            None => panic!("No command..."),
+        }
+        let dist;
+        match data.nth(0) {
+            Some(string) => {
+                match string.parse::<i32>() {
+                    Ok(value) => dist = value,
+                    Err(e) => panic!("Error parsing int: {:?}", e),
+                }
+            },
+            None => panic!("No command value...")
+        }
+        return (cmd, dist) 
+    }).collect();
 }
---
+++ 2021_rust/README.md
@@ -0,0 +1,14 @@
+# Advent  of code 2021
+
+Solution to AoC puzzles 2021, written in rust.
+
+## Run
+
+To run the code for a specific day execute,
+
+1. `cd [folder]`
+2. `cargo run`
+
+## Create rust project
+
+`cargo new aoc_[day] --bin`
